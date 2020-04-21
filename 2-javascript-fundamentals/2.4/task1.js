34eeee0444b08aec57a4ef653b27ec8c2ec2a630
indhu1105 <indhumathi.mani@trustrace.com>
2020-04-21 10:57:25 +0530

    variables declaration

---
+++ 2-javascript-fundamentals/2.4/task1.js
@@ -0,0 +1,14 @@
+/*
+Working with Variables
+
+1.Declare two variables: admin and name.
+2.Assign the value "John" to name.
+3.Copy the value from name to admin.
+4.Show the value of admin using alert (must output “John”).
+*/
+
+let admin;
+let name;
+name="John";
+admin=name;
+alert(admin);
---
+++ 2-javascript-fundamentals/2.4/task2.js
@@ -0,0 +1,21 @@
+
+alert(1);
+// When should we use capitals for a constant and when should we name it normally? 
+/*Being a “constant” just means that a variable’s value never changes.*/
+//constants that are known prior to execution
+const COLOR_RED = "#F00";
+const BIRTHDAY ="18.02.2000";
+alert(BIRTHDAY);
+alert(age);
+/*The value of pageLoadTime is not known prior to the page load, so it’s named normally.*/
+
+//const pageLoadTime=/* time taken by a webpage to load */;
+let value=19;
+age=someCode(value);
+
+someCode(value)
+{
+    alert(20);
+}
+
+//in this age is calculated at run time
